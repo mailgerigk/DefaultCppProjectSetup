257d048a88c4de9a6da02412d2694cf8b76f19ee
gerigk <mail.gerigk@gmail.com>
2023-06-02 21:05:21 +0200

    add VendorDirectory

--- sharpmake/src/Common/BaseConfiguration.cs
+++ sharpmake/src/Common/BaseConfiguration.cs
@@ -20,6 +20,7 @@ public static class BaseConfiguration
     public static string BaseRepositoryDirectory => Path.Combine(DefaultSolutionSharpmakeCsPath, "..", "..", "..");
     public static string SrcDirectory => Path.Combine(BaseRepositoryDirectory, "src");
     public static string BinDirectory => Path.Combine(BaseRepositoryDirectory, "bin");
+	public static string VendorDirectory => Path.Combine(BaseRepositoryDirectory, "vendor");
     public static string SharpmakeOutputDirectory => Path.Combine(BinDirectory, "sharpmake");
 
     public static string ProjectTargetDirectory => Path.Combine(BinDirectory, "[conf.Platform]", "[target.ProjectConfigurationName]");
